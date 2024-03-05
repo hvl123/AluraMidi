be0afcbb4229b0878bfcec507fcbf92f7e060dcb
HenriqueMonteiro <henriquehvl1@gmail.com>
2024-03-05 14:54:24 -0300

    Projeto AluraMidi finalizado

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom(idElementoAudio){
+   const elemento = document.querySelector(idElementoAudio).play();
+   if(elemento != null && elemento.localName === 'audio'){
+    elemento.play()
+} else{
+    console.log('Elemento não encontrado')
+}
+}
+
+  
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (evento){
+    
+        if(evento.code === 'Enter' || evento.code === 'Space' ){
+            tecla.classList.add('ativa');
+        }
+
+    
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
+
+
